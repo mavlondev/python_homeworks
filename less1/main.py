659754ae32f5dbe95d52775eb610f1c405415450
mavlondev <mavlonbekxolnazarov@gmail.com>
2026-05-22 22:57:30 +0500

    try again

--- less1/main.py
+++ less1/main.py
@@ -1,10 +1,9 @@
 print('Hello world!')
  
- # hello world! I'm Mavlonbek Xolnazarov
- # 1. Kompaniyalar uchun eng muhim narsa bu ma'lumotlar, mijozlar haqidagi ma'lumotlar.
- # Bu ma'lumotlarni tahlil qilish orqali xizmatlarni yaxshilashga harakat qilishadi. 
- # 2. Asosan quloy va tushunarli bo'lgan buyruqlar orqali ma'lumotlarni tahlil qilishadi.
- # 3. Bitta kuchli funksiya yordamida barcha ma'lumotlarni soniyalar ichida tahlil qilishadi.
- # 4. Bu haqadi deyarli ma'lumotim yo'q
- # 5. Xizmatlarni yaxshilash uchun ma'lumotlarni tahlil qilishadi, bu esa mijozlarga yanada yaxshi xizmat ko'rsatishga yordam beradi.
- # 6. Yetarli darajada tez emas, C tillari kabi. 
+# 1. The most important thing for companies is data, customer data.
+# They try to improve services by analyzing this data.
+# 2. They analyze data mainly through convenient and understandable commands. It is more modern than Excel and helps to understand it better during analysis.
+# 3. They analyze all the data in seconds with one powerful function. For example, Name, activity, phone number can be stored for each user and these can be used to advertise new products
+# 4. You can do a lot of things with Python libraries. For example, you can prepare images with very high accuracy using the numpy library.
+# 5. You can prepare annual summaries using python and make strong plans for the next year.
+# 6. In the worst case, entering all the data manually would be a pain, or it would be difficult to get help from the community because other programs are not as developed as python
