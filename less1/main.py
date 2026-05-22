faeec5d1d30d7d6e20cebd6f7087dd9df2dbc9dd
mavlondev <mavlonbekxolnazarov@gmail.com>
2026-05-22 22:32:30 +0500

    first lesson done

--- less1/main.py
+++ less1/main.py
@@ -1,1 +1,10 @@
 print('Hello world!')
+ 
+ # hello world! I'm Mavlonbek Xolnazarov
+ # 1. Kompaniyalar uchun eng muhim narsa bu ma'lumotlar, mijozlar haqidagi ma'lumotlar.
+ # Bu ma'lumotlarni tahlil qilish orqali xizmatlarni yaxshilashga harakat qilishadi. 
+ # 2. Asosan quloy va tushunarli bo'lgan buyruqlar orqali ma'lumotlarni tahlil qilishadi.
+ # 3. Bitta kuchli funksiya yordamida barcha ma'lumotlarni soniyalar ichida tahlil qilishadi.
+ # 4. Bu haqadi deyarli ma'lumotim yo'q
+ # 5. Xizmatlarni yaxshilash uchun ma'lumotlarni tahlil qilishadi, bu esa mijozlarga yanada yaxshi xizmat ko'rsatishga yordam beradi.
+ # 6. Yetarli darajada tez emas, C tillari kabi. 
