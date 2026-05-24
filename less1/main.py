99c8e7aa252c27d4dd4f27938cdbc6b65f215873
mavlondev <mavlonbekxolnazarov@gmail.com>
2026-05-24 10:12:35 +0500

    go ahead

--- less1/main.py
+++ less1/main.py
@@ -7,3 +7,32 @@ print('Hello world!')
 # 4. You can do a lot of things with Python libraries. For example, you can prepare images with very high accuracy using the numpy library.
 # 5. You can prepare annual summaries using python and make strong plans for the next year.
 # 6. In the worst case, entering all the data manually would be a pain, or it would be difficult to get help from the community because other programs are not as developed as python
+
+# 1. Companies analyze large volumes of data to make data-driven decisions, understand customer behavior, optimize business operations, and gain a competitive advantage over rivals.
+# 2. While Excel crashes and slows down with large datasets, Python handles millions of rows effortlessly. It automates repetitive tasks through scripts and eliminates human errors.
+# 3. I would build an automated Python pipeline to:
+
+    # Automatically import and clean the daily data.
+
+    # Calculate key metrics (Total Revenue, Top Products).
+
+    # Store it in a database and send a visual report to management
+
+# 4. Python's tasks in BI processes?
+
+    # Data Cleaning (ETL): Fixing missing values and merging different files.
+
+    # Advanced Analytics: Sales forecasting and predictive modeling.
+
+    # Automation: Pulling data automatically from various websites and systems (APIs).
+# 5. Using Python's pandas library, you can group the data by year and sum the profits:
+# df.groupby('Year')['Profit'].sum()
+# Then, you can instantly turn this data into a line or bar chart to see the trend.
+
+# 6. Difficulties without Python?
+
+    # Excel constantly freezing or crashing.
+
+    # Wasting hours on manual copy-pasting and repetitive work.
+
+    # High risk of making manual mistakes in formulas.
