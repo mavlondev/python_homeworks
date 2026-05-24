a0f5ede21c401e8c4d43750d85e275305c065921
mavlondev <mavlonbekxolnazarov@gmail.com>
2026-05-24 10:12:55 +0500

    yes

--- less1/main.py
+++ less1/main.py
@@ -1,12 +1,4 @@
 print('Hello world!')
- 
-# 1. The most important thing for companies is data, customer data.
-# They try to improve services by analyzing this data.
-# 2. They analyze data mainly through convenient and understandable commands. It is more modern than Excel and helps to understand it better during analysis.
-# 3. They analyze all the data in seconds with one powerful function. For example, Name, activity, phone number can be stored for each user and these can be used to advertise new products
-# 4. You can do a lot of things with Python libraries. For example, you can prepare images with very high accuracy using the numpy library.
-# 5. You can prepare annual summaries using python and make strong plans for the next year.
-# 6. In the worst case, entering all the data manually would be a pain, or it would be difficult to get help from the community because other programs are not as developed as python
 
 # 1. Companies analyze large volumes of data to make data-driven decisions, understand customer behavior, optimize business operations, and gain a competitive advantage over rivals.
 # 2. While Excel crashes and slows down with large datasets, Python handles millions of rows effortlessly. It automates repetitive tasks through scripts and eliminates human errors.
